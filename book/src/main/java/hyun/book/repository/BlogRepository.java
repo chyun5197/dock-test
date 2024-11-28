856871beeb17151842a5d7729d710692c74ce0a7
chyun5197 <103193121+chyun5197@users.noreply.github.com>
2024-11-28 19:54:56 +0900

    Delete book directory

--- book/src/main/java/hyun/book/repository/BlogRepository.java
+++
@@ -1,7 +0,0 @@
-package hyun.book.repository;
-
-import hyun.book.domain.Article;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BlogRepository extends JpaRepository<Article, Long> {
-}
